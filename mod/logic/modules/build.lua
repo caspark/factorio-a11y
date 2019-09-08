4dbad10a1b30bb32d6cfbe9d1c0cef9923583511
Caspar Krieger <git@asparck.com>
2019-09-07 23:34:08 -0700

    fix line building not working for inserters and always working for vertical lines even without shift

--- mod/logic/modules/build.lua
+++ mod/logic/modules/build.lua
@@ -33,10 +33,10 @@ end
 
 local function calc_entity_width_and_height(entity_prototype_name)
     local building_prototype = game.entity_prototypes[entity_prototype_name]
-    local width = building_prototype.selection_box.right_bottom.x
-                      - building_prototype.selection_box.left_top.x
-    local height = building_prototype.selection_box.right_bottom.y
-                       - building_prototype.selection_box.left_top.y
+    local width = math.ceil(building_prototype.selection_box.right_bottom.x
+                                - building_prototype.selection_box.left_top.x)
+    local height = math.ceil(building_prototype.selection_box.right_bottom.y
+                                 - building_prototype.selection_box.left_top.y)
     return width, height
 end
 
@@ -79,12 +79,7 @@ end
 
 local function extend_build(player, building_item, building_position, building_direction,
                             shift_build)
-    local building_prototype = game.entity_prototypes[building_item]
-    local width = building_prototype.selection_box.right_bottom.x
-                      - building_prototype.selection_box.left_top.x
-    local height = building_prototype.selection_box.right_bottom.y
-                       - building_prototype.selection_box.left_top.y
-
+    local width, height = calc_entity_width_and_height(building_item)
     local build_history = Game.get_or_set_data("build", player.index, "last_build", false, nil)
 
     reset_build_history(player, {
@@ -97,8 +92,10 @@ local function extend_build(player, building_item, building_position, building_d
                                           building_direction)
     Game.get_or_set_data("build", player.index, "guide_ui_handles", true, guide_handles)
 
-    if shift_build and build_history ~= nil and build_history.position.x == building_position.x
-        or build_history.position.y == building_position.y then
+    if shift_build and build_history ~= nil
+        and (build_history.position.x == building_position.x or build_history.position.y
+            == building_position.y) then
+        player.print('line building triggered')
 
         local offset = Position.subtract(building_position, build_history.position)
         offset = Position.divide(offset, {width, height})
@@ -135,9 +132,6 @@ local function extend_build(player, building_item, building_position, building_d
                                                        Position.multiply(increment, {i, i}),
                                                        {width, height}))
 
-                    -- local check_box = Area.new(building_prototype.selection_box)
-                    -- check_box = Area.offset(check_box, check_pos)
-                    -- draw_block(player, defines.color.green, check_box)
                     player.surface.create_entity{
                         name = 'entity-ghost',
                         position = check_pos,
@@ -146,7 +140,7 @@ local function extend_build(player, building_item, building_position, building_d
                         player = player,
                         raise_built = true,
                         -- ghost specific params
-                        inner_name = building_prototype.name,
+                        inner_name = building_item,
                         expires = false,
                     }
                 end
@@ -161,8 +155,10 @@ function M.register_event_handlers()
     Event.register(defines.events.on_put_item, function(event)
         local player = game.players[event.player_index]
 
-        extend_build(player, player.cursor_stack.name, event.position, event.direction,
-                     event.shift_build)
+        if player.cursor_stack.valid_for_read then
+            extend_build(player, player.cursor_stack.name, event.position, event.direction,
+                         event.shift_build)
+        end
     end)
 
     Event.register(defines.events.on_player_cursor_stack_changed, function(event)
